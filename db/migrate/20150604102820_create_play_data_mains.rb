ba2e3c2944cfd95d05905780f59e702b83aeea9a
h_asa <h-asabatake@m3.com>
2015-06-05 11:11:41 +0000

    delete impressions

--- db/migrate/20150604102820_create_play_data_mains.rb
+++ db/migrate/20150604102820_create_play_data_mains.rb
@@ -4,7 +4,6 @@ class CreatePlayDataMains < ActiveRecord::Migration
     create_table :play_data_mains do |t|
       t.integer    :game_id        #遊んだゲームのId
       t.datetime   :play_datetime  #遊んだ日時
-      t.string     :impressions    #遊んだ時の感想（複数人で共有更新）
       t.timestamps null: false
     end
   end
