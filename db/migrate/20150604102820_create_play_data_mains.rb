4f6bf3100b469428e1c3bc3bbf0140cdf608b83a
h_asa <h-asabatake@m3.com>
2015-06-05 07:28:22 +0000

    add tablee comment

--- db/migrate/20150604102820_create_play_data_mains.rb
+++ db/migrate/20150604102820_create_play_data_mains.rb
@@ -1,8 +1,10 @@
 class CreatePlayDataMains < ActiveRecord::Migration
   def change
+    #ゲームプレイ情報（１人１人の詳細はplay_data_detail
     create_table :play_data_mains do |t|
-      t.integer    :game_id
-      t.datetime   :play_datetime
+      t.integer    :game_id        #遊んだゲームのId
+      t.datetime   :play_datetime  #遊んだ日時
+      t.string     :impressions    #遊んだ時の感想（複数人で共有更新）
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,47 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150604102820) do
-=======
-ActiveRecord::Schema.define(version: 20150604230220) do
->>>>>>> 564acac... add active_admin
+ActiveRecord::Schema.define(version: 20150605072013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
-  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id"
@@ -80,6 +43,16 @@ ActiveRecord::Schema.define(version: 20150604230220) do
     t.string   "pgdb_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "play_data_details", force: :cascade do |t|
+    t.integer  "play_data_main_id"
+    t.integer  "user_id"
+    t.string   "play_score"
+    t.string   "play_winner"
+    t.string   "play_comments"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "play_data_mains", force: :cascade do |t|
